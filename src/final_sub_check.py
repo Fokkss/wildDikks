c6a90b540a0ae7167b05649d66eeb686d4b9f6f0
Fokkss <yuri.v.matveyev@gmail.com>
2026-05-10 03:10:12 +0300

    init repo

---
+++ src/final_sub_check.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+
+
+def save_final_submission(predictions, filename="output/prediction.csv"):
+    df = pd.Series(predictions)
+
+    if df.isnull().values.any():
+        print("NaN detected!!!! :000, turning in 0.0")
+        df = df.fillna(0.0)
+
+    df.to_csv(filename, index=False, header=None, decimal='.', sep=',')
+
+    print(f"Файл {filename} успешно сохранен и готов к отправке!")
+
+
+# test subject
+if __name__ == "__main__":
+    test_data = [10.5, 20.3, 15.0, 90.09]
+    save_final_submission(test_data)
